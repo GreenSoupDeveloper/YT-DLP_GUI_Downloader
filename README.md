79ed6da1dd088096da2b8001fdfeef4d4ebb845d
GreenSoupDev <109924369+GreenSoupDeveloper@users.noreply.github.com>
2024-11-28 23:48:47 -0400

    haha

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # YT-DLP_GUI_Downloader
- gui youtube downloader using yt-dlp
+Lightweight Graphic Youtube Downloader that uses yt-dlp and ffmpeg, pls enjoy lol
+
+it can download in every format
--- YT-DLP_GUI_Downloader/obj/Debug/net8.0-windows7.0/YT-DLP_GUI_Downloader.AssemblyInfo.cs
+++ YT-DLP_GUI_Downloader/obj/Debug/net8.0-windows7.0/YT-DLP_GUI_Downloader.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YT-DLP_GUI_Downloader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8847866623efc6d0c98ca4f79f62aa3bece84f9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("YT-DLP_GUI_Downloader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YT-DLP_GUI_Downloader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
